91eff1e0e810afdd1f05defc169b8106b6c8f715
atthealchemist <at.thealchemist@gmail.com>
2018-12-25 13:14:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Reactorings README
+
+Reactorings - a bunch of useful scaffoldings and refactorings for React in VSCode.
+Something, that missed since Angular Scaffolding and JetBrains IDEs.
+
+### 1.0.0
+
+Initial release of Reactorings
+Now it could:
+- create a new react component (class-based) from html selection
+- create a new react component (const-based) from html selection
+- create an empty react component (class-based) from vscode explorer
+
+### TODOs:
+
+- [ ] add settings
+  - [ ] change jsx extension to js
+  - [ ] add component in folder
+- [ ] some new features
+
+-------------------------------------------------------------------------------------
---
+++ src/extension.ts
@@ -0,0 +1,102 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as path from "path";
+import * as fs from "fs";
+
+
+function getSelectedText() {
+	let activeTextEditor = vscode.window.activeTextEditor;
+	const selection = activeTextEditor.selection;
+	const range = new vscode.Range(new vscode.Position(selection.start.line, selection.start.character), new vscode.Position(selection.end.line, selection.end.character));
+	let selectedText = activeTextEditor.document.getText(range);
+	return selectedText;
+}
+
+function createComponent(isConst) {
+
+	vscode.window.showInputBox({
+		prompt: "Enter name of new component",
+		value: "MyComponent"
+	}).then(value => {
+
+
+		let refactoringContent =
+			`
+	import React, { Component } from 'react';
+	
+	class ${value} extends Component {
+	constructor(props) {
+	super(props);
+	this.state = {}
+	}
+	render() {
+	return (
+		${getSelectedText()}
+	);
+	}
+	}
+	
+	export default ${value};
+	`;
+
+		if (isConst) {
+			refactoringContent =
+`
+const ${value} = props => { return (${getSelectedText()}); };
+
+export default ${value};
+`;
+		}
+
+		let filePath = path.join(vscode.workspace.rootPath, value + '.jsx');
+		fs.writeFileSync(filePath, refactoringContent, 'utf8');
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage(`Extracted HTML into ${value}.jsx`);
+
+
+		vscode.workspace.openTextDocument(filePath).then((doc) => {
+			let options = vscode.window.activeTextEditor.options;
+
+			vscode.window.showTextDocument(doc, 1, false).then(() =>
+				vscode.commands.executeCommand('vscode.executeFormatDocumentProvider', doc.uri, options).then(
+					(changes: vscode.TextEdit[]) => {
+						let formattedEdit = new vscode.WorkspaceEdit();
+						formattedEdit.set(doc.uri, changes);
+						vscode.workspace.applyEdit(formattedEdit);
+						vscode.window.activeTextEditor.document.save();
+					}
+				)
+			);
+		});
+
+	});
+
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	// console.log('Congratulations, your extension "reactorings" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposableNewReactClass = vscode.commands.registerCommand('extension.extractNewReactClass', () => {
+		createComponent(false);
+	});
+
+	let disposableNewReactConst = vscode.commands.registerCommand('extension.extractNewReactConst', () => {
+		createComponent(true)
+	});
+
+	context.subscriptions.push(disposableNewReactClass);
+	context.subscriptions.push(disposableNewReactConst);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
